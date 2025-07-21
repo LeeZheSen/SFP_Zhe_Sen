b70459996abbd0d58fc3ef0575b434b790a7b5ea
LeeZheSen <25072604@imail.sunway.edu.my>
2025-07-21 05:56:11 +0000

    work done for day one

---
+++ Part1/ex05.py
@@ -0,0 +1,8 @@
+# Word Variables 
+Word1 = 'car ' 
+Word2 = 'exceeded '
+Text1 = 'speed limit '
+Word3 = 'the '  
+
+sentence = Word3 + Word1 +Word2 + Word3 + Text1 
+print(sentence) 
---
+++ Part1/ex06.py
@@ -0,0 +1,8 @@
+# List Of Names 
+names = ['Tom', 'Jerry', 'Butch', 'Tuffy', 'Pecos']
+
+print(names)
+print(names[0])
+print(names[-1])
+print(names[1],names[3])
+
